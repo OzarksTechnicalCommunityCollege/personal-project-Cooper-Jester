ccc00566d5bf70849f09a6c48c715f16bb743884
cj0992464 <cj0992464@ITLT19027>
2026-01-20 14:51:12 -0600

    added some comments

--- prsnl/reviews/views.py
+++ prsnl/reviews/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from .models import Game
+from .models import Game, Review
 
 # Create your views here.
 def home(request):
+    #this just links the template and the view function
     games = Game.objects.order_by("-created_at")
     return render(request, "reviews/home.html", {"games": games})
+    #        PLEASE tell me if ^^^this^^^ is correct. i tried just using home.html, but it wouildnt work and i dont know why.
